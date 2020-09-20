a8f036982f6283fb155370cfae87517d68c0b400
MaxJGruber <maxjgruber@gmail.com>
2020-09-20 15:57:03 +0200

    About page done

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 hbs.registerPartials(__dirname + "/views/partials");
+request.open("GET", OMDbAPI, true);
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { css: ["styles"] });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { css: ["styles"] });
 });
 
 app.get("/works", (req, res) => {
